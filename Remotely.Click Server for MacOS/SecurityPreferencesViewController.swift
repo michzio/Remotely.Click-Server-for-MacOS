26f0c26b84fd9dbe9f2b4f43096b18e688ac9cb1
michzio <michzio@hotmail.com>
2017-05-19 15:53:58 +0200

    Preferences and server status menu item completed

--- Remotely.Click Server for MacOS/SecurityPreferencesViewController.swift
+++ Remotely.Click Server for MacOS/SecurityPreferencesViewController.swift
@@ -8,17 +8,71 @@
 
 import Cocoa
 
-class SecurityPreferencesViewController: NSViewController {
+protocol SecurityPreferencesDelegate: class {
+ 
+    func shouldUseSecurityPassword(_ should : Bool, value password: String);
+}
 
+class SecurityPreferencesViewController: NSViewController {
+    
+    weak var delegate : SecurityPreferencesDelegate? = nil;
+
+    @IBOutlet weak var passwordTextField: NSSecureTextField!
+    @IBOutlet weak var confirmPasswordTextField: NSSecureTextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
+    
+        var currentPassword = UserDefaults.standard.string(forKey:"securityPassword") ?? "";
+        passwordTextField.stringValue = currentPassword;
+        confirmPasswordTextField.stringValue = currentPassword;
     }
     
     @IBAction func applyPasswordClick(_ sender: NSButton) {
+        
+        if(passwordTextField.stringValue
+            != confirmPasswordTextField.stringValue) {
+            let alert: NSAlert = NSAlert();
+            alert.messageText = "Password doesn't match";
+            alert.informativeText = "Both password field and confirm password filed should contain the same value.";
+            alert.alertStyle = NSAlertStyle.warning;
+            alert.addButton(withTitle:"OK");
+            alert.runModal();
+            return;
+        }
+        
+        if(passwordTextField.stringValue == "") {
+            let alert: NSAlert = NSAlert();
+            alert.messageText = "Password empty";
+            alert.informativeText = "Password fields left empty. Please enter correct password value.";
+            alert.alertStyle = NSAlertStyle.warning;
+            alert.addButton(withTitle:"OK");
+            alert.runModal(); 
+            return;
+        }
+        
+        // if password & confirm password matches then applay it
+        UserDefaults.standard.set(passwordTextField.stringValue, forKey:"securityPassword");
+        UserDefaults.standard.set(true, forKey:"shouldUseSecurityPassword");
+        
+        delegate?.shouldUseSecurityPassword(true, value: passwordTextField.stringValue);
+        
+        print("Applied password: ", passwordTextField.stringValue);
     }
     
     
     @IBAction func clearPasswordClick(_ sender: NSButton) {
+        
+        UserDefaults.standard.set("", forKey: "securityPassword");
+        UserDefaults.standard.set(false, forKey: "shouldUseSecurityPassword");
+        
+        delegate?.shouldUseSecurityPassword(false, value: "");
+        
+        // clear form controls 
+        passwordTextField.stringValue = "";
+        confirmPasswordTextField.stringValue = "";
+        
+        print("Password cleared");
     }
 }
